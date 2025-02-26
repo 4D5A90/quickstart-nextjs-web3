79c66bbcdd3eab0ea30c87bb85d6608929fb03dd
4D5A90 <50175841+4D5A90@users.noreply.github.com>
2025-02-26 18:43:58 +0100

    upgrade to next15

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function formatAddress(address?: string) {
-  return `${address.slice(0, 6)}...${address.slice(-4)}`;
+  return twMerge(clsx(inputs))
 }
